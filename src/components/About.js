f727fd901ac8b973ba18cc6bf3b3a5a06a75b2ef
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-07-17 13:14:23 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About({about, image = "https://via.placeholder.com/215"}) {
   return (
     <aside>
-        <img src={image} placeholder="https://via.placeholder.com/215" alt='blog logo'/>
+        <img src={image} alt='blog logo'/>
         <p>{about}</p>
     </aside>
   )
